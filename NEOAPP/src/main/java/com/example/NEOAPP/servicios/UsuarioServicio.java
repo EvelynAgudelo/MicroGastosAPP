854965c1410955245a0b38bae8f7bae2f019d5bf
andrea.cuartasrodrg@gmail.com <haseveco@gmail.com>
2026-04-06 08:45:41 -0500

    Se conectaron las dependencias de usuarios

--- NEOAPP/src/main/java/com/example/NEOAPP/servicios/UsuarioServicio.java
+++ NEOAPP/src/main/java/com/example/NEOAPP/servicios/UsuarioServicio.java
@@ -1,17 +1,17 @@
 package com.example.NEOAPP.servicios;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.NEOAPP.repositorios.IUsuarioRepositorio;
+//import com.example.NEOAPP.repositorios.IUsuarioRepositorio;
 
 @Service
 
 public class UsuarioServicio {
 
-    @Autowired //Para conectar las dependencias
+   // @Autowired //Para conectar las dependencias
 
-    private IUsuarioRepositorio repositorio;  //Inyeccion de una dependencia
+    //private IUsuarioRepositorio repositorio;  //Inyeccion de una dependencia
 
     // Servicio para guardar un usuario
 
@@ -23,6 +23,6 @@ public class UsuarioServicio {
     //servicio para modificar un usuario en BD
 
     // //Servicio para buscar un usuario por su id 
-    
+
 
 }
